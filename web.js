98d7d7c329941c6748022d291f3360db5016c06f
Jon Agüera <jonaguera@gmail.com>
2013-07-06 09:47:37 +0200

    Hello world from index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
-
 var app = express.createServer(express.logger());
+var fs = require('fs');
+
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var txt = new Buffer(16);
+  txt = fs.readFileSync('index.html');
+  console.log(txt.toString('utf-8'));
+  response.send(txt.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
